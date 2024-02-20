ae26765317be360ee9f79a64a344e01dcffdc192
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-02-21 01:20:43 +0600

    second-commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,16 @@ myHeading.textContent = "Hello World!";
 
 let myVariable = 'mmm';
 alert(myVariable);
+const imgElement = document.querySelector("img");
+const img1URL = "images/alatoo-logo.png";
+const img2URL = "images\nature-photo.webp";
+function changePhoto(event){
+    let current = imgElement.getAttribute("src");
+    if(current == img1URL){
+        imgElement.setAttribute("src", img2URL);
+    }
+    else{
+        imgElement.setAttribute("src",img1URL);
+    }
+}
+imgElement.onclick = changePhoto;
